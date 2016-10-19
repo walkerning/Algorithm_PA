4d467228715e9d99e8188f48a4435479d4d795d5
walkerning <foxdoraame@gmail.com>
2016-10-19 18:31:19 +0800

    modify test.sh a bit

--- homework3/p3/test.sh
+++ homework3/p3/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 for num in $(echo "10 100 1000 10000 100000 1000000 10000000 100000000 200000000" | tr " " "\n"); do
+    echo "$num" >> result.txt
     echo "$num"
-    ./main $num | grep -v numbers | tee result.txt
+    ./main $num | grep -v numbers | tee tmp_result.txt
+    cat tmp_result.txt >> result.txt
 done
+
+rm tmp_result.txt
